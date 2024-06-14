23d71ec224285007a366750ab606ff47720ec292
Andrey Codez <andreycodez@bixtrix.com>
2024-06-14 17:15:31 +0500

    Updated the test name to express the exact test expectations

--- test/e8.test.js
+++ test/e8.test.js
@@ -1,6 +1,6 @@
-import { describe, expect, it } from "vitest";
-import { getClientWithGreatestBalance } from "../exercises/e8";
-import { bankAccounts } from "../data/data";
+import { describe, expect, it } from 'vitest';
+import { getClientWithGreatestBalance } from '../exercises/e8';
+import { bankAccounts } from '../data/data';
 
 describe('getClientWithGreatestBalance', () => {
   it('Should exist', () => {
@@ -9,15 +9,15 @@ describe('getClientWithGreatestBalance', () => {
   it('Should return an array of 1 item (object)', () => {
     expect(getClientWithGreatestBalance(bankAccounts).length).toEqual(1);
   });
-  it('Should return an exact string value', () => {
+  it('Should return an array with "Joshua" as the object with the highest balance value', () => {
     expect(getClientWithGreatestBalance(bankAccounts)).toEqual([
       {
         id: 3,
         name: 'Joshua',
         balance: 18456.57,
-        deposits: [ 4000, 5000, 6000, 9200, 256.57 ],
-        withdrawals: [ 1500, 1400, 1500, 1500 ]
-      }
+        deposits: [4000, 5000, 6000, 9200, 256.57],
+        withdrawals: [1500, 1400, 1500, 1500],
+      },
     ]);
   });
 });
